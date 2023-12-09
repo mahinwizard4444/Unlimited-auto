4bd1f4887c1faa8cda88484ea22abc888e3e59cb
mahinwizard4444 <86041088+mahinwizard4444@users.noreply.github.com>
2023-12-09 13:01:15 +0530

    Test

---
+++ ViXYZ.js
@@ -0,0 +1,100 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const admin = '918686883838@c.us';
+const admin2 = '917010892470@c.us';
+const Dealer = '2348163376700@c.us';
+
+const groupID1 = '120363132061932497@g.us';
+const groupID2 = '120363186646230726@g.us';
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
+
+client.on('qr', (qr) => {
+    console.log('Scan the QR code to log in:');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+    client.sendMessage(admin, 'Bot Client Initialized...');
+});
+
+let isBotEnabled = false;
+
+let totalCollected = 0
+
+client.on('message', async (message) => {
+    try {
+        if (message.from === groupID1 || message.from === groupID2) {
+            if (message._data.id.participant === Dealer) {
+                console.log('Message received from the dealer');
+                if (isBotEnabled) {
+                    console.log(`Bot is Checking for Captcha match`);
+                    const captchaReceived = message.body.match(/Captcha: (\w+)/);
+                    if (captchaReceived) {
+                        const tierReceived = message.body.match(/🪄 \*Tier:\* (\d+(?:\.\d+)?)/);
+                        console.log(tierReceived);
+                        if (captchaReceived && captchaReceived[1]) {
+                            console.log(`Captcha received: ${captchaReceived[1]}`);
+                            client.sendMessage(
+                                admin,
+                                `Card sent by Dealer [Captcha:${captchaReceived[1]} , Tier:${tierReceived[1]} ]`
+                            );
+                            if (!message.body.match(new RegExp(`#claim ${captchaReceived[1]}`))) {
+                                setTimeout(() => {
+                                    client.sendMessage(message.from, `#claim ${captchaReceived[1]}`);
+                                }, 500);
+
+                                totalCollected += 1
+
+                                console.log(
+                                    `we Claimed the card ${captchaReceived[1]} with Tier ${tierReceived[1]}`
+                                );
+                            } else {
+                                client.sendMessage(admin, 'Someone already claimed');
+                            }
+                        }
+                    } else {
+                        console.error('Invalid captcha format');
+                    }
+                } else {
+                    console.log('Bot is currently disabled');
+                }
+            }
+        }
+    } catch (error) {
+        console.error('An error occurred in Group side:', error);
+    }
+});
+
+// Listen for commands to enable or disable the bot
+client.on('message', (message) => {
+    try {
+        if (message.from === admin) {
+            if (message.body === '/turnOn') {
+                isBotEnabled = true;
+                console.log(`Bot is Listening for Captcha`);
+                client.sendMessage(message.from, 'Bot is now enabled.');
+            } else if (message.body === '/turnOff') {
+                isBotEnabled = false;
+                console.log(`Bot is`);
+                client.sendMessage(message.from, 'Bot is now disabled.');
+            } else if (message.body === '/status') {
+                if (isBotEnabled) {
+                    client.sendMessage(admin, 'Bot is On listening for Captcha');
+                } else {
+                    client.sendMessage(admin, 'Bot is Off Sleep Mode.. /turnOn to start claiming');
+                }
+            }else if(message.body === '/total'){
+                client.sendMessage(message.from, `Total Cards Collected from Turning on is ${t}`)
+            }
+        }
+    } catch (error) {
+        console.error('An error occurred in Admin Msg side:', error);
+    }
+});
+
+client.initialize();
